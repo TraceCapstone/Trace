7c480d8e3343e62808c494851300cb7a1756c3fc
Correy Everett <correy.everett@gmail.com>
2021-03-11 16:37:36 -0600

    working on ApplicationController methods

--- src/main/java/com/trace/trace/contollers/ApplicationController.java
+++ src/main/java/com/trace/trace/contollers/ApplicationController.java
@@ -1,9 +1,36 @@
 package com.trace.trace.contollers;
 
+import com.trace.trace.models.Application;
+import com.trace.trace.repositories.ApplicationRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ApplicationController {
 
+    private final ApplicationRepository applicationDao;
 
+    public ApplicationController(ApplicationRepository applicationDao) {
+        this.applicationDao = applicationDao;
+    }
+
+    //VIEW APPLICATION SUBMISSION FORM
+    @GetMapping("/create-application")
+    public String viewCreateApplicationForm(Model model) {
+        model.addAttribute("application", new Application());
+        return "create-application";
+    }
+
+    //CANCEL FORM SUBMISSION
+    @GetMapping
+    public String cancelFormSubmit() {
+
+    }
+
+    //SAVE APPLICATION FORM
+
+    //EDIT APPLICATION
+
+    //DELETE APPLICATION
 }
--- src/main/java/com/trace/trace/repositories/ApplicationRepository.java
+++ src/main/java/com/trace/trace/repositories/ApplicationRepository.java
@@ -1,4 +1,7 @@
 package com.trace.trace.repositories;
 
-public interface ApplicationRepository {
+import com.trace.trace.models.Application;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
 }
