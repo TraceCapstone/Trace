5e8da206d4c3be7b058989def43dae0f6a884ceb
Connor Wulf <connor.m.wulf@gmail.com>
2021-03-11 13:42:08 -0600

    feat: authentication config

---
+++ src/main/java/com/trace/trace/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.trace.trace.repositories;
+
+import com.trace.trace.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+    User findByUsername(String username);
+}
