1ba41d1b7f194e73983f9f6c428f910e30a54163
jordy muniz <jordy.muniz96@gmail.com>
2021-03-19 11:08:10 -0500

    form popup js

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -26,3 +26,11 @@
         }
     }
 })()
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
