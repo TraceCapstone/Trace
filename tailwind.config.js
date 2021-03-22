2b54c1c521e6bf56df8d0c07882809b7e99e6e34
Connor Wulf <connor.m.wulf@gmail.com>
2021-03-22 16:37:32 -0500

    all applications view styling

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      zIndex: {
+        '-10': '-10',
+      },
+      height: {
+        "6/7": "85.714%"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
